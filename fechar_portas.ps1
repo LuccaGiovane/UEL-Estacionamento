1abb6fb082c1f5f9cc7630ee8627b18c1cd0c959
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2024-08-22 20:29:59 -0300

    Sacola no bolso do pai

---
+++ fechar_portas.ps1
@@ -0,0 +1,9 @@
+# Lista de portas a serem fechadas
+$portas = 8881..8890
+
+foreach ($porta in $portas) {
+    # Remove a regra de entrada para cada porta
+    Get-NetFirewallRule | Where-Object { $_.DisplayName -eq "Abrir Porta $porta" } | Remove-NetFirewallRule
+}
+
+Write-Host "Portas fechadas com sucesso!"
